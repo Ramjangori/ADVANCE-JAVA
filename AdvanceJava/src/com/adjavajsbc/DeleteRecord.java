cf80db5f83da47968d2f6c1550cf1e1c33675e49
MOH RAMJAN <ramjangori6666@gmail.com>
2026-04-13 14:24:17 +0530

    update

--- AdvanceJava/src/com/adjavajsbc/DeleteRecord.java
+++ AdvanceJava/src/com/adjavajsbc/DeleteRecord.java
@@ -10,21 +10,18 @@ public class DeleteRecord {
 	static String DBURL = "jdbc:mysql://localhost:3306/JDBCJAVAA3";
 	static String USER = "root";
 	static String PASS = "Ramjan123";
-	static String  DeleteQuery ="delete from Students where city='City'";
-	
-	
-	
-	
-   public static void main(String[] args) throws ClassNotFoundException, SQLException {
-	Class.forName("com.mysql.cj.jdbc.Driver");
-	
-	Connection con = DriverManager.getConnection(DBURL, USER,PASS );
-	Statement smt = con.createStatement();
-	int d = smt.executeUpdate(DeleteQuery);
-	
-if(d>0) {
-	System.out.println("Delete Sucessfully");
-}
-	
-}
+	static String DeleteQuery = "delete from Students where city='City'";
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection con = DriverManager.getConnection(DBURL, USER, PASS);
+		Statement smt = con.createStatement();
+		int d = smt.executeUpdate(DeleteQuery);
+
+		if (d > 0) {
+			System.out.println("Delete Sucessfully");
+		}
+
+	}
 }
